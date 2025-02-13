c52ecb99b055a4d41735cc78cf4a09c917a8a8ca
Alexxx180 <alexx120702@gmail.com>
2025-02-13 17:48:03 +0300

    find math solution to get inverse from original function

--- menu/model/interface.py
+++ menu/model/interface.py
@@ -1,4 +1,5 @@
-from sympy.abc import x, y
+from sympy.abc import i, x, y
+from sympy import solve
 from common.commander.switch import View
 from common.flow.texts.table import Text
 from menu.table.solutions.solution import RandomDistribution
@@ -16,7 +17,16 @@ def RandomModelReverseMethod(name: str, args: list) -> None:
 	r = integral(express("x * (" + formula + ")"), ab)
 	print("Formula: ", r)
 
+	f1: str = solve(express(formula) - i, x)
+	sol = solve(express(formula + "- i"), x)
+#	f1: str = sol[0].subs(i, x)
+#	f1 = sol
+	print("Inverse: ", f1)
 	f: str = "(x ** 2) * (" + formula + ")"
+# Sol # 1
+# >>> g(x, y)  # g(x, y)
+# >>> _.func(*_.args[::-1])  # g(y, x)
+
 	table = RandomDistribution(args).start()
 	table.expecting = lambda: r
 	table.dispersing = lambda: integral(express(f), ab) - table.math.expect ** 2
